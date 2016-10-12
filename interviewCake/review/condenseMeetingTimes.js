a55f9d7631bbaa5372b38631f078d2bd70e1c0d2
lenny <heliotherapyy@dhcp-10-105-170-218.wireless.northwestern.private>
2016-10-12 15:24:01 -0500

    (add) add files

---
+++ interviewCake/review/condenseMeetingTimes.js
@@ -0,0 +1,7 @@
+var array = [
+    {startTime: 0,  endTime: 1},
+    {startTime: 3,  endTime: 5},
+    {startTime: 4,  endTime: 8},
+    {startTime: 10, endTime: 12},
+    {startTime: 9,  endTime: 10},
+]
