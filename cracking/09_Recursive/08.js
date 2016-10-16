5d887260176cd00e112b61e978b90cc9f44b1aad
lennyKim <heliotherapyy@gmail.com>
2016-10-16 04:36:01 -0500

    (add) add new file

---
+++ cracking/09_Recursive/08.js
@@ -0,0 +1,28 @@
+var denominations = [1, 5, 10, 25];
+var total = 100;
+
+function coinChange(total, denominations) {
+  var cache = {};
+
+  // initialize cache
+  for (var i = 0; i <= total; i++) {
+    cache[i] = 0;
+  }
+
+  var amount = 1;
+  for (i = 0; i < denominations.length; i++) {
+    var coin = denominations[i];
+    for (var amount = coin; amount <= total; amount++) {
+      var remainder = amount - coin;
+      if (remainder === 0) {
+        cache[amount]++;
+      } else {
+        cache[amount] += cache[remainder];
+      }
+    }
+  }
+
+  console.log(cache[total]);
+}
+
+coinChange(30, denominations);
