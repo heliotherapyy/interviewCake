d3a44e9a4f71ed9b9447260a8f34708ba050ded5
lenny <heliotherapyy@dhcp-10-105-94-103.wireless.northwestern.private>
2016-10-18 05:05:08 -0500

    (add) new file

---
+++ interviewCake/review/flightMovies_3.js
@@ -0,0 +1,11 @@
+var flightLength = 100;
+var array = [10,20,70, 32,89,110, 50];
+
+/*
+	The reason this is not DP is that
+	we only pick "two" elements from the array
+	where as coin problem gets accumulated, which makes it possible to DP
+*/
+function main(total, array) {
+
+}
