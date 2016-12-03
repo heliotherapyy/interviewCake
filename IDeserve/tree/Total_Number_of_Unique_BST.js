1914caea04bdda8335e9af055e4efe81cb026666
lennyKim <heliotherapyy@gmail.com>
2016-12-03 17:06:14 -0600

    (fix) optimized

--- IDeserve/tree/Total_Number_of_Unique_BST.js
+++ IDeserve/tree/Total_Number_of_Unique_BST.js
@@ -1,15 +1,27 @@
-var uniqueBSTs = function(value) {
+// Divide and conquer + Memoize
+
+
+var main = function(value) {
+  var cache = {};
+  return uniqueBSTs(value, cache);
+}
+
+var uniqueBSTs = function(value, cache) {
+
+  if (cache[value]) return cache[value];
+
   var total = 0;
   for (var i = 1; i <= value; i++) {
-    total += helper(i, value);
+    total += helper(i, value, cache);
   }
 
+  if (total) cache[value] = total;
   return (!total) ? 1 : total;
 }
 
-var helper = function(root, value) {
-  return uniqueBSTs(root - 1) * uniqueBSTs(value - root);
+var helper = function(root, value, cache) {
+  return uniqueBSTs(root - 1, cache) * uniqueBSTs(value - root, cache);
 }
 
-debugger; var output = uniqueBSTs(1);
+debugger; var output = main(3);
 console.log(output);
